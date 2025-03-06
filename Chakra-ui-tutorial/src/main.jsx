3a0d4dbd38b989934fc3d00d4dfae156100f0f2e
brittbrink <britt@loyyo.com>
2025-03-06 15:57:49 +0100

    Add custom theme with brand colors and fonts to ChakraProvider in main.jsx; update RootLayout background color, lesson 12

--- Chakra-ui-tutorial/src/main.jsx
+++ Chakra-ui-tutorial/src/main.jsx
@@ -2,11 +2,33 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+//extend the theme
+const colors = {
+  brand: {
+    100: '#f7fafc',
+    200: '#edf2f7',
+    300: '#e2e8f0',
+    400: '#cbd5e0',
+    500: '#a0aec0',
+    600: '#718096',
+    700: '#4a5568',
+    800: '#2d3748',
+    900: '#1a202c',
+  },
+}
+
+const fonts = {
+  body: 'Tahoma',
+  heading: 'Courier New',
+}
+
+const theme = extendTheme({ colors, fonts })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
