d83997acec7be3140c46e8363fd872f7c3f461bc
brittbrink <britt@loyyo.com>
2025-03-06 15:16:54 +0100

    Add Profile component with tabs for Account Info and Task History; implement list items with icons, lesson 8

--- Chakra-ui-tutorial/src/pages/Profile.jsx
+++ Chakra-ui-tutorial/src/pages/Profile.jsx
@@ -1,5 +1,62 @@
+import { List, Tab, TabList, TabPanel, TabPanels, Tabs, ListItem, ListIcon } from "@chakra-ui/react";
+import { MdChatBubbleOutline, MdCheckCircle, MdEmail, MdError, MdStar } from "react-icons/md";
+
 export default function Profile() {
   return (
-    <div>Profile</div>
+    <Tabs mt="40px" p="20px" colorScheme="purple" variant="enclosed">
+      <TabList>
+        <Tab _selected={{ color: "white", bg: "purple.400" }}>Account Info</Tab>
+        <Tab _selected={{ color: "white", bg: "purple.400" }}>Task History</Tab>
+      </TabList>
+
+      <TabPanels>
+        <TabPanel>
+          <List spacing={4}>
+            <ListItem>
+              <ListIcon as={MdEmail} color="purple.400" />
+              Email: mario@netninja.dev
+            </ListItem>
+            <ListItem>
+              <ListIcon as={MdChatBubbleOutline} color="purple.400" />
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </ListItem>
+            <ListItem>
+              <ListIcon as={MdStar} color="purple.400" />
+              Lorem ipsum dolor sit amet consectetur.
+            </ListItem>
+          </List>
+        </TabPanel>
+
+        <TabPanel>
+          <List spacing={4}>
+            <ListItem>
+              <ListIcon as={MdCheckCircle} color="teal.400" />
+              Lorem ipsum dolor sit amet consectetur.
+            </ListItem>
+            <ListItem>
+              <ListIcon as={MdCheckCircle} color="teal.400" />
+              Lorem ipsum dolor sit amet consectetur.
+            </ListItem>
+            <ListItem>
+              <ListIcon as={MdError} color="red.400" />
+              Lorem ipsum dolor sit amet consectetur.
+            </ListItem>
+            <ListItem>
+              <ListIcon as={MdCheckCircle} color="teal.400" />
+              Lorem ipsum dolor sit amet consectetur.
+            </ListItem>
+            <ListItem>
+              <ListIcon as={MdError} color="red.400" />
+              Lorem ipsum dolor sit amet consectetur.
+            </ListItem>
+            <ListItem>
+              <ListIcon as={MdCheckCircle} color="teal.400" />
+              Lorem ipsum dolor sit amet consectetur.
+            </ListItem>
+          </List>
+        </TabPanel>
+
+      </TabPanels>
+    </Tabs>
   )
 }
