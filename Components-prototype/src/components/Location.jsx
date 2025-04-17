b6c4757cd24dada61a635dc8f87d045f30601a5f
brittbrink <britt@loyyo.com>
2025-04-17 15:09:37 +0200

    Remove fixed height from iframe in Location component for better responsiveness

--- Components-prototype/src/components/Location.jsx
+++ Components-prototype/src/components/Location.jsx
@@ -67,7 +67,7 @@ export default function Location() {
                                     className="gmap_iframe"
                                     title="Google Map"
                                     src={mapSrc}
-                                    style={{ width: "100%", height: "400px" }}>
+                                    style={{ width: "100%" }}>
                                 </iframe>
                             </AspectRatio>
                         </div>
